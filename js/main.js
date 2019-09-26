159f4555a00bd478e9b43e01af1128ce33deac10
adam Styles <mrfawn@gmail.com>
2019-09-26 20:26:46 +1000

    added vue component for type writer form and v-model to data function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
 // main script
 console.log('main js script loaded');
 
+// vue component - type-writer form
+Vue.component('type-writer', {
+    data: function () {
+      return {
+        formText: 'Add some text here'
+      }
+    },
+    template: `
+    <form>
+        <textarea 
+        name="typeWriterForm" 
+        id="typeWiterForm"
+        cols="30" rows="10"
+        v-model="formText">
+        </textarea>
+    </form>
+    `
+  })
+
 // main vue instance
 let vm = new Vue({
     el: '#app',
     data: {
-        vueTest: 'Vue loaded ok!'
+        vueTest: 'Vue loaded ok!',
     }
 });
