db102e9f934809351a4e41413251c823fd1a755d
adam Styles <mrfawn@gmail.com>
2019-09-26 21:14:59 +1000

    added methods for typewriter ding via howler

--- js/main.js
+++ js/main.js
@@ -4,21 +4,34 @@ console.log('main js script loaded');
 // vue component - type-writer form
 Vue.component('type-writer', {
     data: function () {
-      return {
-        formText: ''
-      } 
+        return {
+            formText: ''
+        }
+    },
+    methods: {
+
+        // play typewriter ding sound via howler js - method
+        alert: function () {
+            console.log('You pressed enter!')
+            // howler js
+            var sound = new Howl({
+                src: ['sound.webm', 'sound.mp3']
+            });
+        }
     },
     template: `
-    <form>
+    <form v-on:submit.prevent="onSubmit">
         <textarea 
         name="typeWriterForm" 
         id="typeWiterForm"
         cols="30" rows="10"
-        v-model="formText">
+        v-model="formText"
+        v-on:keyup.enter="alert"
+        >
         </textarea>
     </form>
     `
-  })
+})
 
 // main vue instance
 let vm = new Vue({
